694246a90c47583445228ce3c394f27c00707aa7
modocode <colonialmediax@gmail.com>
2026-08-10 10:07:11 -0500

    FIX: can now configure noctalia without rebuilding nixos

--- modules/features/noctalia/default.nix
+++ modules/features/noctalia/default.nix
@@ -1,10 +1,19 @@
 { self, inputs, ... }: {
   perSystem = { pkgs, ... }: {
-    packages.myNoctalia = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
-      inherit pkgs; 
-      settings =
-        (builtins.fromJSON
-          (builtins.readFile ./noctalia.json)).settings;
-    };
+
+      packages.myNoctalia =
+    let
+      noctaliaLocation = "home/monad/.config/noctalia";
+    in 
+      inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
+        inherit pkgs;
+        
+        outOfStoreConfig = noctaliaLocation;
+
+        autoCopyConfig = true;
+        settings =
+          (builtins.fromJSON
+            (builtins.readFile ./noctalia.json)).settings;
+      };
   };
 }
